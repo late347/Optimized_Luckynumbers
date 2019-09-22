40b641bb220045ce7c862080e3bfff74d24fe71d
Late347 <late347@gmail.com>
2019-09-23 00:59:50 +0300

    encoding and decoding code probably isnt bugged
    Gotta add customdigitcount func so that the main algorithm stops at correct time
    that was the bug

--- Optimized_Luckynumbers/Optimized_Luckynumbers.cpp
+++ Optimized_Luckynumbers/Optimized_Luckynumbers.cpp
@@ -48,6 +48,24 @@ unsigned int getIntFromEncoding(const std::string& uInt32BitStr) {
 	return returnableres;
 }
 
+unsigned int getCustomDigitCount(const std::string& binaryUint32) {
+	/*calculate how many real digits exists in the binaryUint32
+	discounting emptybits '-'
+	counting '0'
+	counting '1'
+
+	if digitCount returned into the caller, is one more than than the original N digitcount
+	Then we stop looping
+	*/
+	static const int maxBits = 32;
+	unsigned int digitcount = 0;
+	for (size_t i = 0; i < maxBits; i++) {
+		if (binaryUint32[i] == '0' || binaryUint32[i]=='1') {
+			++digitcount;
+		}
+	}
+	return digitcount;
+}
 
 std::string getEncodingFromInt(unsigned int uInt32Bit) {
 	//encode the unsigned int into cppstring
@@ -59,11 +77,6 @@ std::string getEncodingFromInt(unsigned int uInt32Bit) {
 	std::string tempstr;
 
 	//put the binaryDigits into string form e.g. "100101"
-	/*bool b[4];
-int v = 7;  // number to dissect
-
-for (int j = 0;  j < 4;  ++j)
-   b [j] =  0 != (v & (1 << j));*/
 
 	for (size_t i = 0; i < maxBits; i++) {
 		auto temp1 = 0 != (uInt32Bit & (1 << i));
@@ -119,11 +132,15 @@ unsigned int getLuckyNumbers2(unsigned int N) {
 
 	unsigned int luckyint = 0, luckytemp=0,counter = 0;
 	unsigned int luckiesTotal = 0;
-	while (luckyint < N){
+	while (1){
 		luckyint = getIntFromEncoding(luckycandidate); //get the intvalue of the enumerated luckynumber, from encoding
-		counter++; //increment intCounter, from which we get the next enumerated luckynumber
-		luckiesTotal++;
-		luckycandidate = getEncodingFromInt(counter); //store the intCounter encoded format into luckycandidate
+		if (luckyint < N) {
+			counter++; //increment intCounter, from which we get the next enumerated luckynumber
+			luckiesTotal++;
+			luckycandidate = getEncodingFromInt(counter); //store the intCounter encoded format into luckycandidate
+		} else {
+			break;
+		}
 	}
 	return luckiesTotal;
 }
@@ -135,27 +152,7 @@ unsigned int getLuckyNumbers2(unsigned int N) {
 int main()
 {
     std::cout << "Hello World!\n";
-	/*	std::string str;
-	for (size_t i = 0; i < 32; i++) {
-		if (i >= 29 && i<= 31) {
-			str += '0';
-		} else if (i < 29 && i > 22) {
-			str += '1';
-		}
-
-		else {
-			str += '-';
-		}
-		
-	}
-	auto result = getIntFromEncoding(str);
-	std::cout << result << std::endl;
-	int kakka = 4;
-
-	auto result2 = getEncodingFromInt(result);
-	kakka = 33;*/
-
-	auto luckycount = getLuckyNumbers2(1000000);
+	auto luckycount = getLuckyNumbers2(10000);
 	int debug3 = 5;
 	
 
